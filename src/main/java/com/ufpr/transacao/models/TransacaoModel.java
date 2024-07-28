dc572eeb159c8a0755e1a798a5b11235a8434a22
IamThiago-IT <thiagodossantos315@gmail.com>
2024-07-28 20:19:43 -0300

    Add: files

---
+++ src/main/java/com/ufpr/transacao/models/TransacaoModel.java
@@ -0,0 +1,45 @@
+package com.ufpr.transacao.models;
+
+import jakarta.persistence.*;
+import lombok.Date;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TRANSACAO")
+@Data
+public class TransacaoModel implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(unique = true, updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(nullable = false)
+    private Date data;
+
+    @Column(nullable = false)
+    private double valorTransacao;
+
+    @Column(nullable = false)
+    private UUID idCliente;
+
+    @Column(nullable = false)
+    private String tipoTransacao;
+
+    @Column(nullable = false)
+    private UUID idClienteDestinatario;
+
+    @Column(nullable = false)
+    private double saldo;
+
+    @Column
+    private String color;
+}
---
+++ src/test/java/com/ufpr/transacao/TransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufpr.transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
